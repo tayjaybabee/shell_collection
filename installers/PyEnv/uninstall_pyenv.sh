c8ab63fdf00d37247958343ece2f77e198fe861d
tayjaybabee <tayjaybabee@gmail.com>
2020-12-15 12:56:09 -0600

    More code, add installer/uninstaller for PyEnv

---
+++ installers/PyEnv/uninstall_pyenv.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#############################
+
+# File: uninstall_pyenv.sh
+# Author: Taylor-Jayde Blackstone
+# Created: 12/15/2020
+# Description: Uninstalls Pyenv from a system it's installed on.
+
+#############################
+
+INSTALL_PATH=/home/$USER/.pyenv/
+
+read -p "Are you sure you want to uninstall pyenv and any unused dependencies from your system? [N/y]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    to_remove=''
+    echo "Removing $INSTALL_PATH"
+    rm -fr $INSTALL_PATH
+    echo "Removed!"
+    echo "Removing text added to your .bashrc file..."
+    sed -i '\:# < PYENV PATH INFO >:,\:# < /PYENV PATH INFO >:d' ~/.bashrc 
+    echo "Restarting your shell to apply changes to PATH"
+    exec $SHELL
+    echo "Done!"
+    echo "Exiting in"
+    echo
+    echo "3"
+    sleep 1
+    echo
+    echo "2"
+    sleep 1
+    echo
+    echo "1"
+    sleep 1
+    echo
+    echo "Goodbye"
+    clear
+    exit 0
+fi
